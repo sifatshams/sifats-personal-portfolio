c6d030395008e05906f3d9256c45b2aeda2d6e04
Sifat Bin Anwar <sifatshams@outlook.com>
2026-07-18 22:04:31 +0600

    chore: initialize jsconfig.json with dummy cmnt

--- server/src/routes/user.route.js
+++ server/src/routes/user.route.js
@@ -1,20 +1,37 @@
 import express from 'express';
 import {
   deleteAccount,
+  updateNotificationSettingsController,
   updateProfile,
 } from '../controllers/user.controller.js';
 import { getSettingsController } from '../controllers/users/settings.controller.js';
 import { protect } from '../middlewares/auth.middleware.js';
 import { upload } from '../middlewares/upload.middleware.js';
+import validate from '../middlewares/validation.middleware.js';
+import { updateNotificationSettingsValidator } from '../validators/user.validator.js';
 
 const userRoute = express.Router();
 
 // get all user data
 userRoute.get('/settings', protect, getSettingsController);
 
+// update profile
 userRoute.put('/profile', protect, upload.single('avatar'), updateProfile);
 
 // delete acc
 userRoute.delete('/account', protect, deleteAccount);
+
+/**
+ * @route   PATCH /api/users/notification-settings
+ * @desc    Update notification settings
+ * @access  Private (Both Admin & User)
+ */
+userRoute.patch(
+  '/notification-settings',
+  protect,
+  updateNotificationSettingsValidator,
+  validate,
+  updateNotificationSettingsController,
+);
 
 export default userRoute;
