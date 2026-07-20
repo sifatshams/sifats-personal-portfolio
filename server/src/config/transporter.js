bfac5e6909dcdfb355437a104c4c1a1a16ee6307
Sifat Bin Anwar <sifatshams@outlook.com>
2026-07-20 22:17:31 +0600

    fix smtp ipv4 timeout issue

--- server/src/config/transporter.js
+++ server/src/config/transporter.js
@@ -2,18 +2,20 @@ import nodemailer from 'nodemailer';
 
 export const transporter = nodemailer.createTransport({
   host: process.env.BREVO_HOST,
-  port: process.env.BREVO_PORT,
+  port: Number(process.env.BREVO_PORT),
   secure: false,
   auth: {
     user: process.env.BREVO_USER,
     pass: process.env.BREVO_PASS,
   },
+  family: 4, // ipv4 force
+  connectionTimeout: 10000, // 10 second timeout (optional safety)
 });
 
 transporter.verify((error, success) => {
   if (error) {
     console.log(error);
   } else {
-    console.log('SMTP connected successfully');
+    console.log('SMTP connected successfully!');
   }
 });
